df0dc9c973cf3941fdbcb7a3ebb8ace6f58f4bd6
Mohamed <mohamed@aitabderrahman.com>
2016-01-19 21:28:34 +0000

    generate class code and display its name

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import os
 
-retvalue = os.system("sqlacodegen mysql://root:glbtyoung@localhost/mymoney --outfile db.py")
+# generate class for each table, then it create a file that include all generated classes
+retvalue = os.system("sqlacodegen mysql://root:glbtyoung@localhost/mymoney --outfile classes.py")
 
-from db import *
-import db
+from classes import *
+import classes
 import inspect
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-for name, obj in inspect.getmembers(db):
-    if inspect.isclass(obj):
-        print(obj)
+# get all classes from the generated module
+for name, obj in inspect.getmembers(classes):
+    # get a list of generated classes only, without other classes like 'Integer','DateTime'...etc
+    if inspect.isclass(obj) and obj.__module__ == 'classes':
+        # print the class name
+        print(obj.__name__)
 
 print(metadata.tables)
 
+# this part of code can be useful for future use :)
 # engine = create_engine('mysql://root:glbtyoung@localhost/mymoney')
 # engine.connect()
 # Session = sessionmaker(bind=engine)
