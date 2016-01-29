82cde7099e451cacf3259827f82feaa24884b922
Mohamed <mohamed@aitabderrahman.com>
2016-01-29 08:42:51 +0000

    some comments

--- read_file.py
+++ read_file.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# classes module contains sqlacodegen generated classes
 from classes import *
+
+# implement an algorithm for serializing and de-serializing a Python object structure
 import pickle
 
-with open('marks.data', 'rb') as input:
+# you can change these values to test your case :)
+file_to_read = "marks.data"
+
+with open(file_to_read, 'rb') as input:
     obj = pickle.load(input)
     print(obj[0].ID)
     print(obj[0].COURSENAME)
