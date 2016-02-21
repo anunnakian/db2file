dd97f1d29b2050498c254855af7455321bb07575
Mohamed <mohamed@aitabderrahman.com>
2016-02-21 13:41:34 +0000

    bin2xml works.... yaaaay

---
+++ bin2xml.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from utils import *
+import argparse
+import pickle
+import dicttoxml
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract JSON, XML or CSV file from binary data file..')
+    parser.add_argument('-p', '--path', help='Generated binary data file path', required=True)
+    # if this param is not provided, display the result in the default output (the screen :D)
+    parser.add_argument('-o', '--output', help='Output file path', required=False)
+
+    args = parser.parse_args()
+    with open(args.path, 'rb') as input:
+        # loading the list of all stored objects from serialized-file
+        obj = pickle.load(input)
+        print(get_fields(obj[0]))
+        obj = {"data": get_fields(obj[0])}
+
+        xml = dicttoxml.dicttoxml(obj)
+        print xml
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, division, print_function, absolute_import
 import json
 import datetime
+from sqlalchemy.orm.collections import InstrumentedList, InstrumentedDict, InstrumentedSet
 
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
@@ -29,3 +30,24 @@ class AlchemyEncoder(json.JSONEncoder):
             return fields
 
         return json.JSONEncoder.default(self, obj)
+
+
+def get_fields(obj):
+    fields = {}
+    for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+        value = obj.__getattribute__(field)
+        print(type(value))
+        if isinstance(value, datetime.datetime):
+            fields[field] = value.__str__()
+        else:
+            if isinstance(value.__class__, DeclarativeMeta):
+                fields[field] = get_fields(value)
+            else:
+                if isinstance(value, InstrumentedList) or isinstance(value, InstrumentedDict) or isinstance(value, InstrumentedSet):
+                    fields[field] = []
+                    for v in value:
+                        fields[field].append(get_fields(v))
+                else:
+                    fields[field] = value
+    # a json-encodable dict
+    return fields
