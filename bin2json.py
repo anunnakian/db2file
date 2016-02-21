13459c96e604c522160c39ad27979c47c8c935dd
Mohamed <mohamed@aitabderrahman.com>
2016-02-21 12:41:55 +0000

    root element added

--- bin2json.py
+++ bin2json.py
@@ -1,37 +1,7 @@
 # -*- coding: utf-8 -*-
-import json
+from utils import *
 import argparse
 import pickle
-import datetime
-
-from sqlalchemy.ext.declarative import DeclarativeMeta
-
-
-_visited_objects = []
-
-
-class AlchemyEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            # don't re-visit self
-            if obj in _visited_objects:
-                return None
-            _visited_objects.append(obj)
-
-            # an SQLAlchemy class
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                value = obj.__getattribute__(field)
-                if isinstance(value, datetime.datetime):
-                    fields[field] = value.__str__()
-                else:
-                    fields[field] = value
-            # a json-encodable dict
-            return fields
-
-        return json.JSONEncoder.default(self, obj)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract JSON, XML or CSV file from binary data file..')
@@ -43,6 +13,7 @@ if __name__ == '__main__':
     with open(args.path, 'rb') as input:
         # loading the list of all stored objects from serialized-file
         obj = pickle.load(input)
+        obj = {"data":obj}
 
         # if the user specified the output file path
         if args.output:
